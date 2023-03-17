9aea43dc2c5d188891105652223f798210db2719
Ping Fu <ping@michaels.com>
2023-03-17 15:00:40 +0800

    common usage

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -49,14 +49,15 @@ export default async function (req, res) {
 }
 
 function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+return animal;
+//  const capitalizedAnimal =
+//    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
+//  return `Suggest three names for an animal that is a superhero.
+//
+//Animal: Cat
+//Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+//Animal: Dog
+//Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+//Animal: ${capitalizedAnimal}
+//Names:`;
 }
